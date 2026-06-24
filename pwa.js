f1d93d51d52c23a3cc5d3edd93f6be56fa6d3cc6
Jorge Garcia <jorgegarciaficarra@gmail.com>
2026-06-23 21:38:28 -0300

    Agregar soporte PWA instalable

---
+++ pwa.js
@@ -0,0 +1,11 @@
+﻿(function () {
+  "use strict";
+
+  if ("serviceWorker" in navigator) {
+    window.addEventListener("load", function () {
+      navigator.serviceWorker.register("./service-worker.js").catch(function () {
+        // No mostramos error al usuario.
+      });
+    });
+  }
+})();
